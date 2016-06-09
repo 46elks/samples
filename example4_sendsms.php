7122e5e25d72f149b2f7c8b2aa907b538a405b97
Resky277 <dloppo227@gmail.com>
2016-06-10 03:04:26 +0700

    Update example4_sendsms.php

--- example4_sendsms.php
+++ example4_sendsms.php
@@ -6,8 +6,8 @@ function sendSMS ($sms) {
 
   // Set your 46elks API username and API password here
   // You can find them at https://dashboard.46elks.com/
-  $username = 'u2c11ef65b429a8e16ccb1f960d02c734';
-  $password = 'C0ACCEEC0FAFE879189DD5D57F6EC348';
+  $username = 'DALLELOPPO';
+  $password = 'QWERTY2547';
 
   $context = stream_context_create(array(
     'http' => array(
@@ -30,8 +30,8 @@ function sendSMS ($sms) {
 
 
 $sms = array(
-  'from' => 'DummyFrom',   /* Can be up to 11 alphanumeric characters */
-  'to' => '+46709751949',  /* The mobile number you want to send to */
+  'from' => '3',   /* Can be up to 11 alphanumeric characters */
+  'to' => '+628982414777',  /* The mobile number you want to send to */
   'message' => 'Hello hello!'
 );
 echo sendSMS ($sms) . "\n";
